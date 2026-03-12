2e3f4cd873dc6ae6c1dac64a01eb3ad7a777da22
ansh <anshsakariya2810@gmail.com>
2026-03-12 18:58:56 +0530

    project code

--- config/settings.py
+++ config/settings.py
@@ -121,8 +121,9 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STORAGES = {
